11a6be829e7c4b6ea03fec4420a714cd17747b0d
brian101okumu <brian101okumu@gmail.com>
2023-03-16 10:54:00 -0700

    Task 4 hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - is the entry point of this c program
+ * puts - write a line or string to the output
+ * Return:  indicates that the program implementation is accomplished
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+
+
+}
