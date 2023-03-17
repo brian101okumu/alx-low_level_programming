b5bd9fb6ba6203eeb415de8b99b3f51c1b9e2b85
brian101okumu <brian101okumu@gmail.com>
2023-03-17 15:03:13 -0700

    Task 8 print hexadecimal , lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Print all lowercase numbers of base64
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
