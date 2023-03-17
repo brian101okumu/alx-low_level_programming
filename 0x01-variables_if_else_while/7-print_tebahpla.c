7679ac9f5b919659de9aa2d91400a76930f695b9
brian101okumu <brian101okumu@gmail.com>
2023-03-17 14:59:44 -0700

    Task 7 reverse alphabet in lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Print the alphabet in lowercase reverse order
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
