7eddac6ac1a0e8416b8ff2b09301ff1f4088e3d3
brian101okumu <brian101okumu@gmail.com>
2023-03-21 13:18:59 -0700

    Task 3 using ASCII to return lowercase c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - Checks for lowercase letters
+ * Return: int 1 if c is lowercase
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
