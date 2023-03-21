4cd78faf2e9786e92be3b47cbe89edcd747fec30
brian101okumu <brian101okumu@gmail.com>
2023-03-21 12:01:15 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,13 @@
  * main - entry point
  * Return: Always zero
 */
-
 void print_alphabet(void)
 {
 	char b = 'a';
-
 	while (b <= 'z')
 	{
-		_putchar(b);
+		_putchar(b)i;
 		b++;
 	}
-	_putchar('\n');
-
-	
+	_putchar('\n');	
 }
