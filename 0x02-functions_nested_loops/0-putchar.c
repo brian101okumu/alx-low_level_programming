5f18adcbe0c85b387053b91c7ad4d9146215c0bc
brian101okumu <brian101okumu@gmail.com>
2023-03-21 02:38:32 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 to show Success
+ */
+int main(void)
+{
+	char b = "_putchar";
+
+	while (b)
+	{
+		_putchar(b);
+	
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
