037670f54acf346e8bb7455803e1285c8ac670e8
brian101okumu <brian101okumu@gmail.com>
2023-03-21 13:27:50 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _islower - Checks for lowercase letters
- * C: The character to be checked
+ * @C: The character to be checked
  * Return: int 1 if c is lowercase
 */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'a')
 	{
 		return (1);
 	}
