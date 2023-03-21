2a5ac5fc7d7ef941d6ca1e812f8377230aff936d
brian101okumu <brian101okumu@gmail.com>
2023-03-21 12:26:23 -0700

    task 1 spaces

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
-/*
- * print_alphabet - will print the alphabet
+/**
+ * print_alphabet - Prints alphabet
  * Return: void
-*/
+ */
 void print_alphabet(void)
 {
-
 	char b = 'a';
-
-	while (b <= 'z')
+	while ( b <= 'z')
 	{
 		_putchar(b);
 		b++;
 	}
-	_putchar('\n');	
+	_putchar('\n');
 }
