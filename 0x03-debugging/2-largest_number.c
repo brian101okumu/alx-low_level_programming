4a7adf9c36402d933f4448483e7420bede2eb193
brian101okumu <brian101okumu@gmail.com>
2023-03-23 12:58:08 -0700

    Task 2 a is greatest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,16 +6,13 @@
 * @c: third integer
 * Return: largest number
 */
-	int largest_number(int a, int b, int c)
-	{
+int largest_number(int a, int b, int c)
+{
 	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
-	}
-	else if (a > b && c > a)
-	{
-		largest = c;
 	}
 	else if (b > c)
 	{
@@ -25,5 +22,6 @@
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
