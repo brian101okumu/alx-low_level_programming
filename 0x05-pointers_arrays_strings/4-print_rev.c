e86c30090106ac5d5837af25dcda0ab26ffa17ea
brian101okumu <brian101okumu@gmail.com>
2023-03-27 12:48:42 -0700

    Task 4 reversing a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @st: the string 
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int st = 0;
+
+	while (s[st] != '\0')
+	{
+		st++;
+	}
+
+	for (st -= 1; st >=0; st--)
+	{
+		_putchar(s[st]);
+	}
+	_putchar('\n');
+}
